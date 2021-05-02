cd077dfbb21b8fcfbb84840d0a1bc910ac08053c
web-py-g <61905059+web-py-g@users.noreply.github.com>
2021-05-02 12:35:06 +0300

    Upload ful site

---
+++ js/main.js
@@ -0,0 +1,69 @@
+(function () {
+
+    const smoothScroll = function (targetEl, duration) {
+        const headerElHeight =  document.querySelector('.header').clientHeight;
+        let target = document.querySelector(targetEl);
+        let targetPosition = target.getBoundingClientRect().top - headerElHeight;
+        let startPosition = window.pageYOffset;
+        let startTime = null;
+    
+        const ease = function(time,start,target,dur) {
+            time /= dur / 2;
+            if (time < 1) return target / 2 * time * time + start;
+            time--;
+            return -target / 2 * (time * (time - 2) - 1) + start;
+        };
+    
+        const animation = function(currentTime){
+            if (startTime === null) startTime = currentTime;
+            const timeElapsed = currentTime - startTime;
+            const run = ease(timeElapsed, startPosition, targetPosition, duration);
+            window.scrollTo(0,run);
+            if (timeElapsed < duration) requestAnimationFrame(animation);
+        };
+        requestAnimationFrame(animation);
+
+    };
+
+    const scrollTo = function () {
+        const links = document.querySelectorAll('.js-scroll');
+        links.forEach(each => {
+            each.addEventListener('click', function () {
+                const currentTarget = this.getAttribute('href');
+                smoothScroll(currentTarget, 1000);
+            });
+        });
+    };
+    scrollTo();
+}());
+
+
+(function () {
+    const header = document.querySelector('.header');
+    window.onscroll = () => {
+        if(window.pageYOffset > 50){
+            header.classList.add('header__active')
+        } else{
+            header.classList.remove('header__active')
+        }
+    }
+}());
+
+
+$( function() {
+
+if (localStorage.getItem("myKey")) {
+    var stored_select = localStorage.getItem("myKey");
+    $('#' + stored_select).prop( "selected", true );
+    $('.' + stored_select).show();
+    } else {
+        $('.chelyabinsk').show();
+    }
+});
+
+$("#selectItem").change(function(){
+    $('.containerss').find('div').hide();
+    var selected = $('#selectItem option:selected').attr('id');
+    localStorage.setItem("myKey", selected);
+    $('.' + selected).show();
+});
